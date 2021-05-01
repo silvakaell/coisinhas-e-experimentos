9993a92072a7f5d824c07c79ea5e068ff5cecdd1
silvakaell <81882548+silvakaell@users.noreply.github.com>
2021-05-01 00:02:16 -0300

    Update emperortime.py

--- emperortime.py
+++ emperortime.py
@@ -1,3 +1,6 @@
+#com esse script você pode calcular o tempo perdido pelo kurapika do hunter x hunter quando ele usa o emperor time!
+
+
 s = float(input('Olá, Kurapika! Digite quantos segundos você usou o Emperor Time aqui: '))
 hp = s * 1 #hora perdida
 dp = hp/24 #dias perdidos
